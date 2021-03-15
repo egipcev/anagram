42ee32ed4db549977e531c73d00210e5d1cf5351
Oleksii Yehiptsev <egipcev@gmail.com>
2021-03-15 15:33:17 +0200

    implement Task 3 - Maven
    remove pom.xml from gitignore

--- src/main/java/Anagram/Anagram.java
+++ src/main/java/Anagram/Anagram.java
@@ -7,41 +7,41 @@ public class Anagram {
     public static final String DELIMITER = " ";
 
     private String reverseWord(char[] wordToReverse) {
-	int leftIndex = 0;
-	int rightIndex = wordToReverse.length - 1;
-	while (leftIndex <= rightIndex) {
-	    if (Character.isLetter(wordToReverse[leftIndex]) && Character.isLetter(wordToReverse[rightIndex])) {
-		char leftChar = wordToReverse[leftIndex];
-		wordToReverse[leftIndex] = wordToReverse[rightIndex];
-		wordToReverse[rightIndex] = leftChar;
-		leftIndex++;
-		rightIndex--;
-	    } else {
-		if (!Character.isLetter(wordToReverse[leftIndex])) {
-		    leftIndex++;
-		}
-		if (!Character.isLetter(wordToReverse[rightIndex])) {
-		    rightIndex--;
-		}
-	    }
-	}
+        int leftIndex = 0;
+        int rightIndex = wordToReverse.length - 1;
+        while (leftIndex <= rightIndex) {
+            if (Character.isLetter(wordToReverse[leftIndex]) && Character.isLetter(wordToReverse[rightIndex])) {
+                char leftChar = wordToReverse[leftIndex];
+                wordToReverse[leftIndex] = wordToReverse[rightIndex];
+                wordToReverse[rightIndex] = leftChar;
+                leftIndex++;
+                rightIndex--;
+            } else {
+                if (!Character.isLetter(wordToReverse[leftIndex])) {
+                    leftIndex++;
+                }
+                if (!Character.isLetter(wordToReverse[rightIndex])) {
+                    rightIndex--;
+                }
+            }
+        }
 
-	return String.valueOf(wordToReverse);
+        return String.valueOf(wordToReverse);
     }
 
     public String getReversedText(String inputText) {
-	if (inputText == null || inputText.isEmpty()) {
-	    throw new IllegalArgumentException("Invalid input!");
-	} else {
-	    if (inputText.trim().length() == 0)
-		return inputText;
-	    StringJoiner strJoiner = new StringJoiner(DELIMITER);
-	    String[] words = inputText.split(DELIMITER);
-	    for (String word : words) {
-		strJoiner = word.length() == 1 ? strJoiner.add(word) : strJoiner.add(reverseWord(word.toCharArray()));
-	    }
-	    return strJoiner.toString();
-	}
+        if (inputText == null || inputText.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input!");
+        } else {
+            if (inputText.trim().length() == 0)
+                return inputText;
+            StringJoiner strJoiner = new StringJoiner(DELIMITER);
+            String[] words = inputText.split(DELIMITER);
+            for (String word : words) {
+                strJoiner = word.length() == 1 ? strJoiner.add(word) : strJoiner.add(reverseWord(word.toCharArray()));
+            }
+            return strJoiner.toString();
+        }
 
     }
 
--- src/main/java/Anagram/Main.java
+++ src/main/java/Anagram/Main.java
@@ -6,19 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-	Scanner scanner = new Scanner(System.in);
-	System.out.print("Input text to be reversed: ");
-	String inputText = scanner.nextLine();
-	scanner.close();
-	Anagram anagram = new Anagram();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input text to be reversed: ");
+        String inputText = scanner.nextLine();
+        scanner.close();
+        Anagram anagram = new Anagram();
 
-	try {
-	    System.out.print("Reversed text: " + anagram.getReversedText(inputText));
-	} catch (IllegalArgumentException ex) {
-	    System.out.print(ex.getMessage());
-	} catch (RuntimeException ex) {
-	    System.out.print("System error");
-	}
+        try {
+            System.out.print("Reversed text: " + anagram.getReversedText(inputText));
+        } catch (IllegalArgumentException ex) {
+            System.out.print(ex.getMessage());
+        } catch (RuntimeException ex) {
+            System.out.print("System error");
+        }
 
     }
 
